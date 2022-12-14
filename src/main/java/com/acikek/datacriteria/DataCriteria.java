f8f885b9517a5f258fd20639dad76bc020f12f7c
acikek <spiralixp@gmail.com>
2022-12-14 10:02:04 -0800

    move stuff to test mod

--- src/main/java/com/acikek/datacriteria/DataCriteria.java
+++ src/main/java/com/acikek/datacriteria/DataCriteria.java
@@ -1,17 +1,9 @@
 package com.acikek.datacriteria;
 
-import com.acikek.datacriteria.api.DataCriteriaAPI;
-import com.acikek.datacriteria.api.Parameters;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.player.UseBlockCallback;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.List;
 
 public class DataCriteria implements ModInitializer {
 
@@ -25,21 +17,6 @@ public class DataCriteria implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
-            if (player instanceof ServerPlayerEntity serverPlayer) {
-                DataCriteriaAPI.trigger(
-                        new Identifier("datacriteria:use_block"), // ID of criterion
-                        true, serverPlayer,
-                        Parameters.block((ServerWorld) serverPlayer.world, hitResult.getBlockPos()), // block predicate parameter
-                        player.getStackInHand(hand) // item predicate parameter
-                );
-                DataCriteriaAPI.trigger(
-                        new Identifier("datacriteria:list_of_ints"),
-                        true, serverPlayer,
-                        List.of(1, 2, 3, 4, 5)
-                );
-            }
-            return ActionResult.PASS;
-        });
+        LOGGER.info("Initializing Data Criteria...");
     }
 }
---
+++ test/src/main/java/com/acikek/test/dctest/DataCriteriaTest.java
@@ -0,0 +1,39 @@
+package com.acikek.test.dctest;
+
+import com.acikek.datacriteria.api.DataCriteriaAPI;
+import com.acikek.datacriteria.api.Parameters;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Identifier;
+
+import java.util.List;
+
+public class DataCriteriaTest implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+            if (player instanceof ServerPlayerEntity serverPlayer) {
+                DataCriteriaAPI.trigger(
+                        new Identifier("datacriteria:use_block"), // ID of criterion
+                        true, serverPlayer,
+                        Parameters.block((ServerWorld) serverPlayer.world, hitResult.getBlockPos()), // block predicate parameter
+                        player.getStackInHand(hand) // item predicate parameter
+                );
+            }
+            return ActionResult.PASS;
+        });
+
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            DataCriteriaAPI.trigger(
+                    new Identifier("datacriteria:list_of_ints"),
+                    true, handler.player,
+                    List.of(1, 2, 3, 4, 5)
+            );
+        });
+    }
+}
